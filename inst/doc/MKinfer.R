b3aec83b3bb31f17c120c93101f3561b722bc70b
Matthias Kohl <Matthias.Kohl@stamats.de>
2020-01-08 08:20:10 +0000

    version 0.4

--- inst/doc/MKinfer.R
+++ inst/doc/MKinfer.R
@@ -1,7 +1,7 @@
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(MKinfer)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## default: "wilson"
 binomCI(x = 12, n = 50)
 ## Clopper-Pearson interval
@@ -9,13 +9,13 @@ binomCI(x = 12, n = 50, method = "clopper-pearson")
 ## identical to 
 binom.test(x = 12, n = 50)$conf.int
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## default: wilson 
 binomDiffCI(a = 5, b = 0, c = 51, d = 29)
 ## default: wilson with continuity correction
 binomDiffCI(a = 212, b = 144, c = 256, d = 707, paired = TRUE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 x <- rnorm(50, mean = 2, sd = 3)
 ## mean and SD unknown
 normCI(x)
@@ -28,7 +28,7 @@ normCI(x, mean = 2, alternative = "less")
 ## bootstrap
 meanCI(x, boot = TRUE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 x <- rnorm(20)
 y <- rnorm(20, sd = 2)
 ## paired
@@ -51,7 +51,7 @@ normDiffCI(x, y, method = "classical", boot = TRUE, bootci.type = "bca")
 ## bootstrap: assuming unequal variances
 normDiffCI(x, y, method = "welch", boot = TRUE, bootci.type = "bca")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 M <- 100
 CIhsu <- CIwelch <- CIclass <- matrix(NA, nrow = M, ncol = 2)
 for(i in 1:M){
@@ -69,7 +69,7 @@ sum(CIwelch[,1] < 0 & 0 < CIwelch[,2])/M
 ## Hsu
 sum(CIhsu[,1] < 0 & 0 < CIhsu[,2])/M
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 x <- rnorm(100, mean = 10, sd = 2) # CV = 0.2
 ## default: "miller"
 cvCI(x)
@@ -78,7 +78,7 @@ cvCI(x, method = "gulhar")
 ## bootstrap
 cvCI(x, method = "boot")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 x <- rexp(100, rate = 0.5)
 ## exact
 quantileCI(x = x, prob = 0.95)
@@ -87,7 +87,7 @@ quantileCI(x = x, prob = 0.95, method = "asymptotic")
 ## boot
 quantileCI(x = x, prob = 0.95, method = "boot")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## exact
 medianCI(x = x)
 ## asymptotic
@@ -95,10 +95,10 @@ medianCI(x = x, method = "asymptotic")
 ## boot
 medianCI(x = x, method = "boot")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 medianCI(x = x, minLength = TRUE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## exact
 madCI(x = x)
 ## aysymptotic
@@ -108,7 +108,7 @@ madCI(x = x, method = "boot")
 ## unstandardized
 madCI(x = x, constant = 1)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 t.test(1:10, y = c(7:20))      # P = .00001855
 t.test(1:10, y = c(7:20, 200)) # P = .1245    -- NOT significant anymore
 hsu.t.test(1:10, y = c(7:20))
@@ -121,7 +121,7 @@ with(sleep, hsu.t.test(extra[group == 1], extra[group == 2]))
 t.test(extra ~ group, data = sleep)
 hsu.t.test(extra ~ group, data = sleep)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 boot.t.test(1:10, y = c(7:20)) # without bootstrap: P = .00001855
 boot.t.test(1:10, y = c(7:20, 200)) # without bootstrap: P = .1245
 
@@ -130,7 +130,7 @@ with(sleep, boot.t.test(extra[group == 1], extra[group == 2]))
 ## Formula interface
 boot.t.test(extra ~ group, data = sleep)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 perm.t.test(1:10, y = c(7:20)) # without permutation: P = .00001855
 ## permutation confidence interval sensitive to outlier!
 perm.t.test(1:10, y = c(7:20, 200)) # without permutation: P = .1245
@@ -140,7 +140,7 @@ with(sleep, perm.t.test(extra[group == 1], extra[group == 2]))
 ## Formula interface
 perm.t.test(extra ~ group, data = sleep)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ## Generate some data
 set.seed(123)
 x <- rnorm(25, mean = 1)
@@ -181,7 +181,7 @@ mi.t.test(res$imputations, x = "variable", mu = -1, subset = D$group == "yes",
 t.test(D$variable, D$pair, paired = TRUE)
 mi.t.test(res$imputations, x = "variable", y = "pair", paired = TRUE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 set.seed(123)
 outcome <- c(rnorm(10), rnorm(10, mean = 1.5), rnorm(10, mean = 1))
 timepoints <- factor(rep(1:3, each = 10))
@@ -191,13 +191,35 @@ rm.oneway.test(outcome, timepoints, patients, method = "lme")
 rm.oneway.test(outcome, timepoints, patients, method = "friedman")
 rm.oneway.test(outcome, timepoints, patients, method = "quade")
 
-## ------------------------------------------------------------------------
+## ---- fig.width=7, fig.height=7-----------------------------------------------
+## Generate some data
+x <- matrix(rnorm(1000, mean = 10), nrow = 10)
+g1 <- rep("control", 10)
+y1 <- matrix(rnorm(500, mean = 11.75), nrow = 10)
+y2 <- matrix(rnorm(500, mean = 9.75, sd = 3), nrow = 10)
+g2 <- rep("treatment", 10)
+group <- factor(c(g1, g2))
+Data <- rbind(x, cbind(y1, y2))
+## compute Hsu t-test
+pvals <- apply(Data, 2, function(x, group) hsu.t.test(x ~ group)$p.value,
+               group = group)
+## compute log-fold change
+logfc <- function(x, group){
+  res <- tapply(x, group, mean)
+  log2(res[1]/res[2])
+}
+lfcs <- apply(Data, 2, logfc, group = group)
+volcano(lfcs, p.adjust(pvals, method = "fdr"), 
+        effect.low = -0.25, effect.high = 0.25, 
+        xlab = "log-fold change", ylab = "-log10(adj. p value)")
+
+## -----------------------------------------------------------------------------
 SD1 <- c(0.149, 0.022, 0.036, 0.085, 0.125, NA, 0.139, 0.124, 0.038)
 SD2 <- c(NA, 0.039, 0.038, 0.087, 0.125, NA, 0.135, 0.126, 0.038)
 SDchange <- c(NA, NA, NA, 0.026, 0.058, NA, NA, NA, NA)
 imputeSD(SD1, SD2, SDchange)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 pairwise.wilcox.test(airquality$Ozone, airquality$Month, 
                      p.adjust.method = "none")
 ## To avoid the warnings
@@ -205,6 +227,6 @@ library(exactRankTests)
 pairwise.fun(airquality$Ozone, airquality$Month, 
              fun = function(x, y) wilcox.exact(x, y)$p.value)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 sessionInfo()
 
